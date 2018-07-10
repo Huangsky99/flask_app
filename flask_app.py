2a51b419f82be5bc4fec67ea8a9d1eb25cb46625
huangsky <huangsky_99@163.com>
2018-07-10 06:21:41 -0700

    commit ori

---
+++ flask_app.py
@@ -0,0 +1,42 @@
+from flask import render_template, flash, redirect
+from flask import Flask
+from forms import LoginForm
+
+app = Flask(__name__)
+app.config.from_object("config")
+
+
+@app.route('/')
+@app.route('/index')
+def hello_world():
+
+    user = {"nickname": "telen"}
+
+    posts = [
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+
+    return render_template("index.html", title="Home", user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="' + form.openid.data + '", remember_me=' + str(form.remember_me.data))
+        return redirect('/index')
+
+    return render_template('login.html', title='Sign In', form=form)
+
+
+if __name__ == '__main__':
+    app.run()
